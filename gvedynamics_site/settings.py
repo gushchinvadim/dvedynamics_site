e1012187d905a5a176231075aa3b8c15d5fb4b6f
Vadim Gushchin <gushchinvadim@gmail.com>
2025-09-30 09:41:22 +0300

    fourth commit

--- gvedynamics_site/settings.py
+++ gvedynamics_site/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 import os
+from email.policy import default
 from pathlib import Path
 from decouple import config
 
@@ -74,10 +75,15 @@ WSGI_APPLICATION = 'gvedynamics_site.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST', default='localhost'),
+        'PORT': config('DB_PORT', default='5432'),
     }
 }
 
